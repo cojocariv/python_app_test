7575dc90d1c6b26ca36fd4c9b5a737e57a3cd77d
Vasilii Cojocari <cojocari.v88@gmail.com>
2022-10-06 08:37:27 +0300

    as

---
+++ main.py
@@ -0,0 +1,13 @@
+from calendar import calendar
+
+
+print("hello to calendar \n")
+
+year=int(input("introduceti anul: "))
+
+month=int(input("introduceti luna: "))
+
+print(calendar.month(year, month))
+
+print("goodbye")
+
